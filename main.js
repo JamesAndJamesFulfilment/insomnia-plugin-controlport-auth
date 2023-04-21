67e8424f445e65d670b6be2de8fd0469ab67f67c
Halfpint <alexander.sims92@gmail.com>
2023-04-21 12:40:47 +0100

    Add template tags for setting either the HEADER or BODY strategy

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 // For help writing plugins, visit the documentation to get started:
 //   https://docs.insomnia.rest/insomnia/introduction-to-plugins
-
-// TODO: Add plugin code here...
 const md5 = require('md5')
 
 const halfApiKeyGenerator = (apiKey) => {
@@ -18,39 +16,53 @@ const halfApiKeyGenerator = (apiKey) => {
 
 module.exports.templateTags = [
     {
-        name: 'security_hash_base64',
-        displayName: 'Deepwell Bearer token using half_api_key',
-        description: 'Generate a base64 encoded string represneting our half_api_key auth strategy',
+        name: 'controlport_api_authentication',
+        displayName: 'ControlPort API authentication',
+        description: 'Either signs a Bearer token in the Authorization header or injects our half_api_key authentication payload into the request body.',
+        args: [
+            {
+                displayName: 'Strategy',
+                deaultValue: 'HEADER',
+                type: 'enum',
+                options: [
+                    { 
+                        displayName: 'Authorization Header', 
+                        value: 'HEADER', 
+                        description: 'Generate a base64 encoded string representing the half_api_key strategy' 
+                    },
+                    { 
+                        displayName: 'Request Body', 
+                        value: 'BODY', 
+                        description: 'Inject our half_api_key_strategy into the request body' 
+                    },
+                ]
+            },
+        ],
 
-        async run (context) {
-            const apiKey = context.context['api_key']
-            const authMethod = context.context['auth_method']
-
-            if (authMethod.toUpperCase() !== 'HEADER') {
-                return ''
-            }
-
-            return Buffer.from(halfApiKeyGenerator(apiKey)).toString('base64')
+        async run (context, strategy) {
+            return strategy
         },
     },
 ]
 
 module.exports.requestHooks = [
   async context => {
-        const apiKey = context.request.getEnvironmentVariable('api_key')
-        const authMethod = context.request.getEnvironmentVariable('auth_method')
+        const apiKey = context.request.getEnvironmentVariable('controlport_api_key')
+        const authMethod = context.request.getEnvironmentVariable('controlport_auth_method')
+        console.log('method is', authMethod)
 
-        if (authMethod.toUpperCase() !== 'BODY') {
-            return
+        if (authMethod === 'BODY') {
+            const body = context.request.getBody();
+            let authPayload = JSON.parse(halfApiKeyGenerator(apiKey))
+            if (body.text) {
+                const text = JSON.parse(body.text)
+                authPayload = { ...text, ...authPayload }
+            }
+
+            context.request.setBody({ ...body, text: JSON.stringify(authPayload) })
+        } else if (authMethod === 'HEADER') {
+            context.request.setHeader('Authorization', `Bearer ${Buffer.from(halfApiKeyGenerator(apiKey)).toString('base64')}`)
         }
 
-        const body = context.request.getBody();
-        let authPayload = JSON.parse(halfApiKeyGenerator(apiKey))
-        if (body.text) {
-            const text = JSON.parse(body.text)
-            authPayload = { ...text, ...authPayload }
-        }
-
-        context.request.setBody({ ...body, text: JSON.stringify(authPayload) })
   }
 ];
