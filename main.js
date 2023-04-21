0df5468d69484ac95b044ffa510e6148e564f4f2
Halfpint <alexander.sims92@gmail.com>
2023-04-21 12:06:56 +0100

    Support both header and body auth mechanisms

--- main.js
+++ main.js
@@ -4,22 +4,53 @@
 // TODO: Add plugin code here...
 const md5 = require('md5')
 
-module.exports.templateTags = [{
-    name: 'security_hash',
-    displayName: 'Deepwell Security Hash Generator',
-    description: 'Create security hashes based off a clients API key',
+const halfApiKeyGenerator = (apiKey) => {
+    const halfApiKey       = apiKey.substring(0, 16)
+    const messageTimestamp = Math.floor((new Date().getTime()) / 1000)
+    const securityHash     = md5(`${messageTimestamp}${apiKey}`)
 
-    async run (context) {
-        let apiKey  = context.context['api_key']
+    return JSON.stringify({
+        half_api_key: halfApiKey,
+        message_timestamp: messageTimestamp,
+        security_hash: securityHash
+    })
+}
 
-        let halfApiKey       = apiKey.substring(0, 16)
-        let messageTimestamp = Math.floor((new Date().getTime()) / 1000)
-        let securityHash     = md5(`${messageTimestamp}${apiKey}`)
+module.exports.templateTags = [
+    {
+        name: 'security_hash_base64',
+        displayName: 'Deepwell Bearer token using half_api_key',
+        description: 'Generate a base64 encoded string represneting our half_api_key auth strategy',
 
-        return JSON.stringify({
-            half_api_key: halfApiKey,
-            message_timestamp: messageTimestamp,
-            security_hash: securityHash
-        })
+        async run (context) {
+            const apiKey = context.context['api_key']
+            const authMethod = context.context['auth_method']
+
+            if (authMethod.toUpperCase() !== 'HEADER') {
+                return ''
+            }
+
+            return Buffer.from(halfApiKeyGenerator(apiKey)).toString('base64')
+        },
     },
-}]
+]
+
+module.exports.requestHooks = [
+  async context => {
+        const apiKey = context.request.getEnvironmentVariable('api_key')
+        const authMethod = context.request.getEnvironmentVariable('auth_method')
+
+        if (authMethod.toUpperCase() !== 'BODY') {
+            return
+        }
+
+        const body = context.request.getBody();
+        let authPayload = JSON.parse(halfApiKeyGenerator(apiKey))
+        if (body.text) {
+            const text = JSON.parse(body.text)
+            authPayload = { ...text, ...authPayload }
+        }
+
+        context.request.setBody({ ...body, text: JSON.stringify(authPayload) })
+  }
+];
